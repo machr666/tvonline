f4dc515a51834b86ed1caa67c707b2c45d7321bc
machr666 <machr666@machr666-desktop.(none)>
2013-03-26 09:14:30 +0000

    Changed style slightly and added more content to the config page

--- templates/myStyle/static/js/nav.js
+++ templates/myStyle/static/js/nav.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-	
+
     // Elements in document
     var $content = $("#content"),
         $nav = $("#nav"),
         $load = $("#load");
-    
+
     // This function changes the content
     var updatePage = function(state) {
-        $content.hide('fast', function() {
+        $content.slideUp('fast', function() {
             $load.fadeIn('fast');
             $.get(state, function (data) {
                 var tempHTML=data.replace(/<script/g, "<dynscript").replace(/<\/script/g, "</dynscript");
-	        cnt=$(tempHTML).find("#content").html().replace(/dynscript/g, "script");
-		$content.html(cnt);
+                cnt=$(tempHTML).find("#content").html().replace(/dynscript/g, "script");
+                $content.html(cnt);
                 $load.fadeOut('normal');
-                $content.show('normal');
-	    });
+                $content.slideDown('normal');
+            });
         });
     }
 
