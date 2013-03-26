703a0d590e68b75d293aea565fe0013a440b624f
machr666 <machr666@machr666-desktop.(none)>
2013-03-26 16:11:14 +0000

    Login and logout now implemented. Next up is the configuration handler.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os.path
+
 import tornado.web
 import tornado.httpserver
 import tornado.options
@@ -12,20 +13,57 @@ class TVServer(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", ReqHandler),
-            (r"/([a-zA-Z0-9-_]*\.html)", ReqHandler),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
+            (r"/(\w+)", ReqHandler),
         ]
+        mainDir = os.path.dirname(__file__)
         settings = dict(
-            template_path = os.path.join(os.path.dirname(__file__), "templates/myStyle"),
-            static_path = os.path.join(os.path.dirname(__file__), "templates/myStyle/static"),
-            debug = True,
+            xsrf_cookie = True,
+            cookie_secret = "bls9+x7PT5GIbaBuKzsGOecL9SG7KUmEh6rNbMYTpfk=",
+            login_url = "/login",
+            template_path = os.path.join(mainDir, "templates/myStyle"),
+            static_path = os.path.join(mainDir, "templates/myStyle/static"),
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 
-class ReqHandler(tornado.web.RequestHandler):
-    ''' Handle get requests '''
-    def get(self, page="home.html"):
-        print("Loading " + page)
-        self.render("../"+page)
+class PersonalisedRequestHandler(tornado.web.RequestHandler):
+    ''' This class specifies how we store the user identity '''
+
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+    def delete_session_cookie(self):
+        self.clear_cookie("user");
+
+class LoginHandler(PersonalisedRequestHandler):
+    ''' Show the login page and handle authentication process '''
+
+    def get(self):
+        if not self.current_user:
+            self.render("../login.html")
+            return
+        self.redirect("/")
+
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("username"))
+        self.redirect("/")
+
+class LogoutHandler(PersonalisedRequestHandler):
+    ''' End the user's current session '''
+
+    def get(self):
+        self.delete_session_cookie()
+        self.redirect("/")
+
+class ReqHandler(PersonalisedRequestHandler):
+    ''' Handle get/post requests for the TVOnline website '''
+
+    @tornado.web.authenticated
+    def get(self, page="home"):
+        html = page + ".html"
+        print("Loading " + html)
+        self.render("../" + html)
 
 # Launch server
 if __name__ == "__main__":
